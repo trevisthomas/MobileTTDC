1b31521ef9f5f4b6826fd0735656f6e94c13b1b6
Trevis Thomas <trevisthomas@gmail.com>
2016-05-28 12:46:14 -0500

    Initial Commit

---
+++ MobileTTDC/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MobileTTDC
+//
+//  Created by Trevis Thomas on 5/28/16.
+//  Copyright © 2016 Trevis Thomas. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
